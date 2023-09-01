c3b57f093a7c69ee87028cf807833119aaf41050
Mohamed El Hedi HAMDI <mh.hamdi@sofrecom-tunisie.com>
2023-09-01 22:30:42 +0100

    first commit

---
+++ src/main/java/com/mohamedhedi1/VisualSecure/VisualSecureApplication.java
@@ -0,0 +1,13 @@
+package com.mohamedhedi1.VisualSecure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisualSecureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisualSecureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mohamedhedi1/VisualSecure/VisualSecureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohamedhedi1.VisualSecure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisualSecureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
